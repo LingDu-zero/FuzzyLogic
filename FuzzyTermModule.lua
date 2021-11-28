f7ac5ce38c040f39dac9c4b0f1d9d820640582bb
LingDu-zero <33028145+LingDu-zero@users.noreply.github.com>
2021-11-28 22:36:15 +0800

    Create FuzzyTermModule.lua

---
+++ FuzzyTermModule.lua
@@ -0,0 +1,19 @@
+local FuzzyTermModule = class('FuzzyTermModule')
+
+function FuzzyTermModule:initialize()
+
+end
+
+function FuzzyTermModule:ClearDOM()
+
+end
+
+function FuzzyTermModule:GetDOM()
+	
+end
+
+function FuzzyTermModule:ORwithDOM(var)
+	
+end
+
+return FuzzyTermModule
